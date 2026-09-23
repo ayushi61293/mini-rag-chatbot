84900c86ac18308eaa5b3e9194941e9115ef4d6f
ayushi61293 <143417734+ayushi61293@users.noreply.github.com>
2026-09-23 21:33:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ async def load_models():
         embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
-        )
+        
         print("✅ Embeddings loaded")
 
         api_key = os.getenv("GROQ_API_KEY")
